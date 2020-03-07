a9fb34f32cdf8c918e014094a1f25382ae6ffc85
Beatriz Barroso <beatrizscnbarroso@gmail.com>
2020-03-07 21:15:29 +0000

    first stages of styling news feed

--- magicmirror/mirrorv1/views.py
+++ magicmirror/mirrorv1/views.py
@@ -10,9 +10,12 @@ def index(request):
         'week_day' : getWeekDayName(datetime.datetime.now().isoweekday()),
         'datetime' : datetime.datetime.now()
     }
-    getNews()
+    news = getNews()
     weather = getWeather(request)
-    return render(request, 'mirrorv1/index.html', context = {'city_time' : city_time, 'weather' : weather})
+    return render(request, 'mirrorv1/index.html', 
+    context = {'city_time' : city_time, 
+                'weather' : weather,
+                'news' : news})
 
 def getWeekDayName(day_of_week):
     days_of_week = {
@@ -53,21 +56,7 @@ def getNews() :
     # Init
     newsapi = NewsApiClient(api_key='8cef481256e4410fbacdad418e928efc')
 
-    # /v2/top-headlines
-    top_headlines = newsapi.get_top_headlines(sources='bbc-news,the-verge',
+    top_headlines = newsapi.get_top_headlines(sources='bbc-news',
                                             language='en',
-                                            page_size=5)
-
-    # /v2/everything
-    # all_articles = newsapi.get_everything(q='bitcoin',
-    #                                     sources='bbc-news,the-verge',
-    #                                     domains='bbc.co.uk,techcrunch.com',
-    #                                     from_param='2017-12-01',
-    #                                     to='2017-12-12',
-    #                                     language='en',
-    #                                     sort_by='relevancy',
-    #                                     page=2)
-
-    # /v2/sources
-    # sources = newsapi.get_sources()
+                                            page_size=3)
     return top_headlines
